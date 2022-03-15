5316a4f9b4fe6045749998283e30847ec3e79576
Economou, Matthew (NIH/NIAID) [C] <economoum@niaid.nih.gov>
2022-03-15 16:01:04 -0400

    feat(docker-library): adapt manifest file generator from https://github.com/docker-library/python

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,181 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+declare -A aliases=(
+	[8.0]='8 latest'
+)
+
+self="$(basename "$BASH_SOURCE")"
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+if [ "$#" -eq 0 ]; then
+	versions="$(jq -r 'keys | map(@sh) | join(" ")' versions.json)"
+	eval "set -- $versions"
+fi
+
+# sort version numbers with highest first
+IFS=$'\n'; set -- $(sort -rV <<<"$*"); unset IFS
+
+# get the most recent commit which modified any of "$@"
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
+dirCommit() {
+	local dir="$1"; shift
+	(
+		cd "$dir"
+		files="$(
+			git show HEAD:./Dockerfile \
+				| awk '
+					toupper($1) == "COPY" {
+						for (i = 2; i < NF; i++) {
+							if ($i ~ /^--from=/) {
+								next
+							}
+							print $i
+						}
+					}
+					'
+			 )"
+		fileCommit Dockerfile $files
+	)
+}
+
+# TODO: remove unsupported architectures (e.g., arm32, i386, mips, ppc, s390)
+getArches() {
+	# exclude this image (e.g., oneself) from the architecture lookup
+	local repo="$1"; shift
+	# search this place for information on parent images
+	local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/'
+
+	# loop through the generated Dockerfiles and grab the list of
+	# suported hardware architectures of the parent image (excluding
+	# 'scratch' and $repo)
+	eval "declare -g -A parentRepoToArches=(
+		$(
+			find -name 'Dockerfile' -exec awk '
+				toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|.*\/.*)(:|$)/ {
+					print "'"$officialImagesUrl"'" $2
+				}
+				' '{}' + \
+				| sort -u \
+				| xargs bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
+		)
+	)"
+}
+getArches 'satosa'
+
+cat <<-EOH
+# This file is generated via https://github.com/ibrsp/satosa/blob/$(fileCommit "$self")/$self
+
+Maintainers: Matthew X. Economou <economoum@niaid.nih.gov> (@niheconomoum)
+GitRepo: https://github.com/ibrsp/satosa.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+
+for version; do
+	export version
+	variants="$(jq -r '.[env.version].variants | map(@sh) | join(" ")' versions.json)"
+	eval "variants=( $variants )"
+
+	fullVersion="$(jq -r '.[env.version].version' versions.json)"
+
+	versionAliases=(
+		$fullVersion
+		$version
+		${aliases[$version]:-}
+	)
+
+	defaultDebianVariant="$(jq -r '
+				.[env.version].variants
+				| map(select(
+						startswith("alpine")
+						or startswith("slim-")
+						| not
+				))
+				| .[0]
+		' versions.json)"
+	defaultAlpineVariant="$(jq -r '
+				.[env.version].variants
+				| map(select(
+						startswith("alpine")
+				))
+				| .[0]
+		' versions.json)"
+
+	for v in "${variants[@]}"; do
+		dir="$version/$v"
+		[ -f "$dir/Dockerfile" ] || continue
+		variant="$(basename "$v")"
+
+		commit="$(dirCommit "$dir")"
+
+		variantAliases=( "${versionAliases[@]/%/-$variant}" )
+		case "$variant" in
+			*-"$defaultDebianVariant") # slim-xxx -> slim
+				variantAliases+=( "${versionAliases[@]/%/-${variant%-$defaultDebianVariant}}" )
+				;;
+			"$defaultAlpineVariant")
+				variantAliases+=( "${versionAliases[@]/%/-alpine}" )
+				;;
+		esac
+		variantAliases=( "${variantAliases[@]//latest-/}" )
+
+		case "$v" in
+			# FIXME: this broke with after modifying the variant naming scheme
+			windows/*)
+				variantArches='windows-amd64'
+				;;
+
+			*)
+				variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
+				variantArches="${parentRepoToArches[$variantParent]}"
+				;;
+		esac
+
+		sharedTags=()
+		for windowsShared in windowsservercore nanoserver; do
+			if [[ "$variant" == "$windowsShared"* ]]; then
+				sharedTags=( "${versionAliases[@]/%/-$windowsShared}" )
+				sharedTags=( "${sharedTags[@]//latest-/}" )
+				break
+			fi
+		done
+
+		if [ "$variant" = "$defaultDebianVariant" ] || [[ "$variant" == 'windowsservercore'* ]]; then
+			sharedTags+=( "${versionAliases[@]}" )
+		fi
+
+		case "$variant" in
+			*py3.10*)
+				# https://github.com/docker-library/python/issues/682
+				variantArches="$(sed -r -e 's/ mips64le / /g' <<<" $variantArches ")"
+				;;
+		esac
+
+		# FIXME: filter out unsupported archictectures here?
+		variantArches="amd64"
+
+		echo
+		echo "Tags: $(join ', ' "${variantAliases[@]}")"
+		if [ "${#sharedTags[@]}" -gt 0 ]; then
+			echo "SharedTags: $(join ', ' "${sharedTags[@]}")"
+		fi
+		cat <<-EOE
+			Architectures: $(join ', ' $variantArches)
+			GitCommit: $commit
+			Directory: $dir
+		EOE
+		if [[ "$v" == windows/* ]]; then
+			echo "Constraints: $variant"
+		fi
+	done
+done
